086d1e51247d26546a4381ac46d578709738bc0f
Stelian Adrian Stanci <UO277653@uniovi.es>
2022-05-12 17:46:59 +0200

    Fixes

--- sdi-entrega2-test-21/src/main/java/socialnetwork/SocialNetworkApplicationTests.java
+++ sdi-entrega2-test-21/src/main/java/socialnetwork/SocialNetworkApplicationTests.java
@@ -26,11 +26,11 @@ class SocialNetworkApplicationTests {
 //    static String Geckodriver = "C:\\Users\\Alejandro\\Desktop\\SDI-2022\\software\\software\\geckodriver-v0.27.0-win64\\geckodriver.exe";
 
     // Adrian
-    //static String Geckodriver = "C:\\Users\\adria\\OneDrive\\Escritorio\\UNIVERSIDAD\\AÑO 3\\SEMESTRE 2\\Sistemas Distribuidos e Internet\\Laboratorio\\Lab5\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe";
+    static String Geckodriver = "C:\\Users\\adria\\OneDrive\\Escritorio\\UNIVERSIDAD\\AÑO 3\\SEMESTRE 2\\Sistemas Distribuidos e Internet\\Laboratorio\\Lab5\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe";
 
 
     //Sara
-    static String Geckodriver = "D:\\UNI\\3º\\2º cuatri\\SDI\\Lab\\sesion05\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe";
+    //static String Geckodriver = "D:\\UNI\\3º\\2º cuatri\\SDI\\Lab\\sesion05\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe";
 
     //Diego
 //    static String Geckodriver = "C:\\Users\\dimar\\Desktop\\sdi\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe";
@@ -909,6 +909,7 @@ class SocialNetworkApplicationTests {
                     .append("surname", surname)
                     .append("password", "test")
                     .append("role", role));
+            mongoClient.close();
         } catch (MongoException me) {
             System.err.println("Unable to insert due to an error: " + me);
         }
